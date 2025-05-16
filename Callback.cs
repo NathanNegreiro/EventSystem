98871ea220243f5f3c36772156a4e85077a284ce
NathanNegreiro <nathan.negreiro@gmail.com>
2025-05-16 10:38:56 -0700

    Initial Upload of EventSystem

---
+++ Callback.cs
@@ -0,0 +1,15 @@
+﻿/*******************************************************************************/
+/*!
+\file   Callback.cs
+\author Nathan Negreiro
+\par    All content © Nathan Negreiro, all rights reserved.
+
+\brief
+  Provides templated types to use with messaging
+  
+*/
+/*******************************************************************************/
+public delegate void Callback();
+public delegate void Callback<T>(T arg1);
+public delegate void Callback<T, U>(T arg1, U arg2);
+public delegate void Callback<T, U, V>(T arg1, U arg2, V arg3);
